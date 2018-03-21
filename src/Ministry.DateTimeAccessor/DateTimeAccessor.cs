07574e075403be863091a583542700463e7c0a3c
Keith Jackson <keith@ministryotech.co.uk>
2018-03-21 17:29:51 +0000

    Initial commit

---
+++ src/Ministry.DateTimeAccessor/DateTimeAccessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+// ReSharper disable once CheckNamespace
+namespace Ministry
+{
+    #region | Interface |
+
+    /// <summary>
+    /// Accesses Date & Time information.
+    /// </summary>
+    public interface IDateTimeAccessor
+    {
+        /// <summary>
+        /// Gets the current Date & Time
+        /// </summary>
+        DateTime Now { get; }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Accesses Date & Time information.
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class DateTimeAccessor : IDateTimeAccessor
+    {
+        /// <summary>
+        /// Gets the current Date & Time
+        /// </summary>
+        public DateTime Now => DateTime.Now;
+    }
+}
---
+++ src/Ministry.DateTimeAccessor/DateTimeHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+// ReSharper disable once CheckNamespace
+namespace Ministry
+{
+    /// <summary>
+    /// Extension Methods for working with DateTime objects.
+    /// </summary>
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// The Unix epoch for calculations since 1970.
+        /// </summary>
+        /// <value>
+        /// The generally accepted minimum DateTime for dating digital resources.
+        /// </value>
+        private static DateTime MinUnixTime => new DateTime(1970, 1, 1, 0, 0, 0);
+
+        /// <summary>
+        /// Applies a Gmt offset to the DateTime.
+        /// </summary>
+        /// <param name="time">The time without gmt.</param>
+        /// <returns>The input time with the computer's current gmt offset applied.</returns>
+        public static DateTime ToGmtTime(this DateTime time)
+            => time - (DateTime.Now.AddDays(1) - DateTime.UtcNow.AddDays(1));
+
+        /// <summary>
+        /// Applies a Gmt offset to the DateTime.
+        /// </summary>
+        /// <param name="time">The time without gmt.</param>
+        /// <returns>The input time with the computer's current gmt offset applied.</returns>
+        public static DateTime FromGmtTime(this DateTime time)
+            => time - (DateTime.UtcNow.AddDays(1) - DateTime.Now.AddDays(1));
+
+        /// <summary>
+        /// Calculates the number of seconds since 1970, a Unix based date format.
+        /// </summary>
+        /// <remarks>Converts the input <see cref="ToGmtTime"/> to account for the computer.</remarks>
+        /// <param name="time">The time to calculate seconds for.</param>
+        /// <returns>The number of seconds since 1970.</returns>
+        public static long ToUnixEpochTime(this DateTime time)
+            => (long)time.ToGmtTime().Subtract(MinUnixTime).TotalSeconds;
+
+        /// <summary>
+        /// Calculates the number of seconds since 1970, a Unix based date format.
+        /// </summary>
+        /// <remarks>Converts the input <see cref="ToGmtTime"/> to account for the computer.</remarks>
+        /// <param name="time">The time to calculate seconds for.</param>
+        /// <returns>The number of seconds since 1970.</returns>
+        public static DateTime FromUnixEpochTime(this int seconds)
+            => MinUnixTime.AddSeconds(seconds);
+    }
+}
